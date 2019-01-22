2977920a20b9c1c2dc8fd0b611b83c269395331f
GroggyGrunt <46584112+GroggyGrunt@users.noreply.github.com>
2019-01-22 21:10:30 +0100

    Update SSDchip.ino

--- SSDchip/SSDchip.ino
+++ SSDchip/SSDchip.ino
@@ -1,10 +1,20 @@
 #include <Wire.h>
+#include "SevSeg.h"
+SevSeg sevseg; 
 
 int bpm = 0;
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+  byte numDigits = 4;
+  byte digitPins[] = {10, 11, 12, 13};
+  byte segmentPins[] = {9, 2, 3, 5, 6, 8, 7, 4};
+  bool resistorsOnSegments = true; 
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_CATHODE; 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
 }
 
 void loop() {
@@ -15,5 +25,7 @@ void loop() {
     Serial.println(bpm);         // print the character
   }
 
-  delay(250);
+  sevseg.setNumber(bpm);
+  sevseg.refreshDisplay();
+  //delay(250);
 }
